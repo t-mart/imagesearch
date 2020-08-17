aab9dd0723cb95272a2b499bed9c3edccf8d4ac3
Tim Martin <tim@timmart.in>
2020-08-17 18:52:41 -0500

    Bump version: 0.2.1 → 0.2.2

--- imagesearch/__init__.py
+++ imagesearch/__init__.py
@@ -1,7 +1,7 @@
 """
 Returns the similiarity between a reference image and a set of other images.
 """
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from imagesearch.fingerprint import Algorithm, ImageFingerprint
 from imagesearch import exceptions
